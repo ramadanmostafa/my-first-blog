da94c0c19d9abbe8c8e2326dd974331fcedd7455
ramadan <ramadan@codescalers.com>
2016-12-23 13:26:58 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    url(r'^post/(?P<pk>\d+)/$', views.post_details, name="post_details"),
     url(r'^$', views.post_list, name="post_list"),
 ]
